dfd7173b82f571a4868e9ab9aed4fe6c787fec93
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-22 22:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function validEmail(str) {
-  //your JS code here.
+  let reg = /^[^.\s]+$/;
+  let arr = str.split("@")[0];
+  let arr1 = str.split("@")[1];
+  if (!reg.test(arr) || !arr1 || !reg.test(arr1.split(".")[0]) || !arr1.split(".")[1]) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 // Do not change the code below.
